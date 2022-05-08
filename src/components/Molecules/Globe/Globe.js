0a61e0edcc967760ba56fd972e3185a4e8dd0123
Roeland van Stee <roelandvanstee@gmail.com>
2022-05-08 13:02:25 +0200

    Add test click events

--- src/components/Molecules/Globe/Globe.js
+++ src/components/Molecules/Globe/Globe.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Circle, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 import BlueMap from "../../../img/blue-world.png";
@@ -11,9 +11,15 @@ import "./Globe.scss";
 const Globe = () => {
     return (
         <div className="globe">
-            <Canvas>
-                <ambientLight intensity={0.4} color="#fff" />
-                <OrbitControls rotateSpeed={0.1} />
+            <Canvas onPointerMissed={() => console.log("missed")}>
+                <ambientLight intensity={0.35} color="#ffffff" />
+                <OrbitControls
+                    rotateSpeed={0.1}
+                    autoRotate
+                    autoRotateSpeed={0.2}
+                    enableZoom={false}
+                />
+                <PerspectiveCamera makeDefault position={[1, 0, 2.5]} />
                 <GlobeModel />
             </Canvas>
         </div>
@@ -24,9 +30,30 @@ const GlobeModel = () => {
     const globeMap = useLoader(TextureLoader, BlueMap);
 
     return (
-        <mesh>
-            <sphereBufferGeometry args={[3, 64, 64]} />
+        <mesh
+            onClick={(e) => {
+                e.stopPropagation();
+                console.log("yo");
+            }}
+        >
+            <Patch />
+            <sphereBufferGeometry args={[1, 32, 32]} />
             <meshPhongMaterial map={globeMap} />
+        </mesh>
+    );
+};
+
+const Patch = (name) => {
+    return (
+        <mesh
+            onClick={(e) => {
+                e.stopPropagation();
+                console.log("clicvk");
+            }}
+            position={[0.7, 0.56, 0.55]}
+        >
+            <sphereBufferGeometry args={[0.055, 32, 32]} />
+            <meshBasicMaterial color="#01cbe1" />
         </mesh>
     );
 };
