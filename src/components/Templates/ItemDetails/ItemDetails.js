8e701120ed6277b2a5a35fe780684a45203b98c6
Roeland van Stee <roelandvanstee@gmail.com>
2022-05-09 17:39:52 +0200

    Add fist styling to item detail page

--- src/components/Templates/ItemDetails/ItemDetails.js
+++ src/components/Templates/ItemDetails/ItemDetails.js
@@ -9,9 +9,20 @@ const ItemDetails = () => {
     const item = itemsData.find((item) => item.url == itemUrl);
 
     return (
-        <div className="ItemDetails">
-            <h1>{item.title}</h1>
-            <p></p>
+        <div className="item-details">
+            <div className="item-details__images">
+                <figure className="item-details__figure">
+                    <img
+                        className="item-details__big-image"
+                        src={item.image}
+                        alt=""
+                    />
+                </figure>
+            </div>
+            <section className="item-details__info">
+                <h1 className="item-details__title">{item.title}</h1>
+                <p className="item-details__intro">{item.intro}</p>
+            </section>
         </div>
     );
 };
