d68755156b8942b817246c2bb623e6f8001b0176
Roeland van Stee <roelandvanstee@gmail.com>
2022-05-21 14:01:55 +0200

    Add ScrollToTop functionalityu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { createContext, useContext, useState } from "react";
 
 import { Route, Routes } from "react-router-dom";
 
+import ScrollToTop from "./utils/ScrollToTop";
+
 import GlobeTemplate from "./components/Templates/GlobeTemplate/GlobeTemplate";
 import ItemDetailTemplate from "./components/Templates/ItemDetailTemplate/ItemDetailTemplate";
 import OverviewTemplate from "./components/Templates/OverviewTemplate/OverviewTemplate";
@@ -12,20 +14,24 @@ export const ActivePatchContext = React.createContext();
 
 const App = () => {
     const [activePatch, setActivePatch] = useState("pacific");
-
     return (
         <div className="app">
             <ActivePatchContext.Provider
                 value={{ activePatch, setActivePatch }}
             >
-                <Routes>
-                    <Route path="/" exact element={<GlobeTemplate />} />
-                    <Route
-                        path="overview"
-                        element={<OverviewTemplate />}
-                    ></Route>
-                    <Route path=":itemUrl" element={<ItemDetailTemplate />} />
-                </Routes>
+                <ScrollToTop>
+                    <Routes>
+                        <Route path="/" exact element={<GlobeTemplate />} />
+                        <Route
+                            path="overview"
+                            element={<OverviewTemplate />}
+                        ></Route>
+                        <Route
+                            path=":itemUrl"
+                            element={<ItemDetailTemplate />}
+                        />
+                    </Routes>
+                </ScrollToTop>
             </ActivePatchContext.Provider>
         </div>
     );
