3c7f382942393de5b714d5abc0e95ccdc8274064
Roeland van Stee <roelandvanstee@gmail.com>
2022-05-30 11:00:31 +0200

    Add label to icon

--- src/components/Atoms/Navigation/Navigation.jsx
+++ src/components/Atoms/Navigation/Navigation.jsx
@@ -5,22 +5,31 @@ import GlobeIcon from "../../../img/icons/globe-icon.svg";
 
 import "./Navigation.scss";
 
-const Navigation = ({icon}) => {
+const Navigation = ({ icon }) => {
     return (
         <nav className="navigation">
             {icon === "overview" ? (
-                <Link to="overview" className="navigation__link">
+                <Link
+                    to="overview"
+                    className="navigation__link navigation__link--grid"
+                >
                     <div className="navigation__icon">
                         <div className="navigation__icon-inner"></div>
                         <div className="navigation__icon-inner"></div>
                         <div className="navigation__icon-inner"></div>
                         <div className="navigation__icon-inner"></div>
                     </div>
+                    <p className="navigation__icon-label">All items</p>
                 </Link>
             ) : (
                 <Link to="/" className="navigation__link">
                     <div className="navigation__icon">
-                        <img className="navigation__globe" src={GlobeIcon} alt="" />
+                        <img
+                            className="navigation__globe"
+                            src={GlobeIcon}
+                            alt=""
+                        />
+                        <p className="navigation__icon-label">Globe</p>
                     </div>
                 </Link>
             )}
