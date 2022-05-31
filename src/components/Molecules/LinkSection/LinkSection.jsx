4b37d4a46c23d5252e3daaf8383d6610384ce585
Roeland van Stee <roelandvanstee@gmail.com>
2022-05-31 23:03:22 +0200

    Add real link to section

--- src/components/Molecules/LinkSection/LinkSection.jsx
+++ src/components/Molecules/LinkSection/LinkSection.jsx
@@ -10,53 +10,92 @@ const LinkSection = () => {
             <h3 className="link-section__title">Learn more</h3>
             <ul className="link-section__list">
                 <li className="link-section__item">
-                    <a className="link-section__link" href="#">
+                    <a
+                        className="link-section__link"
+                        href="https://theoceancleanup.com/oceans/"
+                        target="_blank"
+                    >
                         <img
                             className="link-section__arrow"
                             src={ArrowRight}
                             alt=""
                         />
-                        The recycling process of the plastic
+                        How we clean the oceans
                     </a>
                 </li>
+
                 <li className="link-section__item">
-                    <a className="link-section__link" href="#">
+                    <a
+                        className="link-section__link"
+                        href="https://theoceancleanup.com/rivers/"
+                        target="_blank"
+                    >
                         <img
                             className="link-section__arrow"
                             src={ArrowRight}
                             alt=""
                         />
-                        The recycling process of the plastic
+                        How we clean the rivers
                     </a>
                 </li>
+
                 <li className="link-section__item">
-                    <a className="link-section__link" href="#">
+                    <a
+                        className="link-section__link"
+                        href="https://theoceancleanup.com/updates/category/recycling/"
+                        target="_blank"
+                    >
                         <img
                             className="link-section__arrow"
                             src={ArrowRight}
                             alt=""
                         />
-                        The recycling process of the plastic
+                        What happens to the plastic
                     </a>
                 </li>
+
                 <li className="link-section__item">
-                    <a className="link-section__link" href="#">
+                    <a
+                        className="link-section__link"
+                        href="https://theoceancleanup.com/donate/"
+                        target="_blank"
+                    >
                         <img
                             className="link-section__arrow"
                             src={ArrowRight}
                             alt=""
                         />
-                        The recycling process of the plastic
+                        Help us clean the oceans by donating
                     </a>
                 </li>
+
                 <li className="link-section__item">
-                    <a className="link-section__link" href="#">
+                    <a
+                        className="link-section__link"
+                        href="https://theoceancleanup.com/updates/category/recycling/"
+                        target="_blank"
+                    >
                         <img
                             className="link-section__arrow"
                             src={ArrowRight}
                             alt=""
                         />
-                        The recycling process of the plastic
+                        Most recent updates from The Ocean Cleanup
+                    </a>
+                </li>
+
+                <li className="link-section__item">
+                    <a
+                        className="link-section__link"
+                        href="https://theoceancleanup.com/plastic-tracker/"
+                        target="_blank"
+                    >
+                        <img
+                            className="link-section__arrow"
+                            src={ArrowRight}
+                            alt=""
+                        />
+                        See where your plastic trash would end up
                     </a>
                 </li>
             </ul>
