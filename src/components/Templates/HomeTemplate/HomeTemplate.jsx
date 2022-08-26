8ad26cde00559b86d0e55bac845dad58a7d8c0bd
Roeland van Stee <roelandvanstee@gmail.com>
2022-08-26 17:32:01 +0200

    Add font mixins

--- src/components/Templates/HomeTemplate/HomeTemplate.jsx
+++ src/components/Templates/HomeTemplate/HomeTemplate.jsx
@@ -4,8 +4,22 @@ import "./HomeTemplate.scss";
 
 const HomeTemplate = () => {
     return (
-        <section className="globe-template">
-            <h1>Yo</h1>
+        <section className="home-template">
+            <h1 className="home-template__title">
+                Hallo dit is een grote titel
+            </h1>
+            <h2 className="home-template__second-title">
+                Hallo dit is een medium titel
+            </h2>
+            <h2 className="home-template__uppercase-title">
+                Hallo dit is een uppercase titel
+            </h2>
+
+            <p className="home-template__body-text">
+                Lopende tekst. Etiam sit amet orci eget eros faucibus tincidunt.
+                Ut leo. Praesent adipiscing. Duis arcu tortor, suscipit eget,
+                imperdiet nec, imperdiet iaculis, ipsum.
+            </p>
         </section>
     );
 };
